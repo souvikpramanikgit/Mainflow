e47e34d0252f06e4899205b733a42366d2f25a63
Souvik Kumar Pramanik <123581988+souvikpramanikgit@users.noreply.github.com>
2024-06-01 02:58:59 +0530

    Add files

---
+++ Task 3/script.js
@@ -0,0 +1,91 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    checkInputs();
+});
+
+const sendData = (sRate, count) => {
+    if (sRate === count) {
+        swal("Welcome!", "Registration successful", "success").then(() => {
+            // Refresh the page after showing the success message
+            setTimeout(() => {
+                location.reload();
+            }, 1000);
+        });
+    }
+}
+const successMsg = () => {
+    let formCon = document.getElementsByClassName('form-control');
+    var count = formCon.length - 1;
+    for (var i = 0; i < formCon.length; i++) {
+        if (formCon[i].className === "form-control success") {
+            var sRate = 0 + i;
+            sendData(sRate, count);
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+function checkInputs() {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else if (usernameValue.length <= 2) {
+        setErrorFor(username, 'Username min 3 character');
+    }
+    else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+    } else if (passwordValue.length <= 3) {
+        setErrorFor(password, 'Min 4 character');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (password2Value === '') {
+        setErrorFor(password2, 'Password cannot be blank');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Passwords does not match');
+    } else {
+        setSuccessFor(password2);
+    }
+    successMsg();
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
